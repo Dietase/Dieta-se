f728d1064e4ad5586adab6a55ab17f9ee94f1c6c
Dietase <dietase.app@gmail.com>
2025-11-30 19:33:07 -0300

    Minor update

--- test_api.php
+++ test_api.php
@@ -1,3 +1,14 @@
 <?php
+// Headers CORS completos
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
-echo json_encode(['status' => 'ok']);
+
+// Responder requisições OPTIONS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+echo json_encode(['status' => 'ok', 'message' => 'API Railway funcionando!']);
